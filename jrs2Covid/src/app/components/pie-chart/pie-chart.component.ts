b77aca13e138954ff8ff881a2e6a68b5af732c6a
Will Austell <62250017+Twaustell@users.noreply.github.com>
2021-09-29 10:34:41 -0400

    refactored pie chart

--- jrs2Covid/src/app/components/pie-chart/pie-chart.component.ts
+++ jrs2Covid/src/app/components/pie-chart/pie-chart.component.ts
@@ -19,6 +19,8 @@ export class PieChartComponent implements OnInit {
 
   data;
 
+
+
   constructor(private worldService: WorldometersService) { }
 
   ngOnInit(): void {
@@ -39,34 +41,101 @@ export class PieChartComponent implements OnInit {
       .attr("height", this.height + (this.margin * 2))
       .append("g")
       .attr("transform", "translate(" + this.width / 2 + "," + this.height / 2 + ")")
+    this.svg.append("text")
+      .attr("text-anchor", "middle")
+      .text("South Carolina Covid Cases")
+      .style("font-size", 25)
   }
 
   drawChart() {
-    const pie = d3.pie<any>().value((d: any) => d);
+
+    // const pie = d3.pie<any>().value((d: any) => d);
 
     const total = this.data.population;
     const partial = this.data[this.value]
 
 
-    this.svg
-      .append('path')
-      .attr('d', d3.arc()
-        .startAngle(0)
-        .endAngle(2 * Math.PI)
-        .innerRadius(this.radius * 0.6)
-        .outerRadius(this.radius)
-      )
-      .attr('fill', "#ddd");
+    // this.svg
+    //   .append('path')
+    //   .attr('d', d3.arc()
+    //     .startAngle(0)
+    //     .endAngle(2 * Math.PI)
+    //     .innerRadius(this.radius * 0.6)
+    //     .outerRadius(this.radius)
+    //   )
+    //   .attr('fill', "#ddd");
+
+    // this.svg
+    //   .append('path')
+    //   .attr('d', d3.arc()
+    //     .startAngle(0)
+    //     .endAngle((partial / total) * (2 * Math.PI))
+    //     .innerRadius(this.radius * 0.6)
+    //     .outerRadius(this.radius)
+    //   )
+    //   .attr('fill', "red")
+
+    var pie = d3.pie()
+      .sort(null) // Do not sort group by size
+      .value(function (d) { return d.value; })
+    // var data_ready = pie([this.data.cases, this.data.population - this.data.cases])
+
+    var color = d3.scaleLinear()
+      .domain([-1, 0, 1])
+      .range(["red", "white", "green"]);
+
+    var data_ready = pie([{ key: "population", value: this.data.population }, { key: "cases", value: this.data.cases }])
+
+    var arc = d3.arc()
+      .innerRadius(this.radius * 0.5)         // This is the size of the donut hole
+      .outerRadius(this.radius * 0.8)
+
+
+    var outerArc = d3.arc()
+      .innerRadius(this.radius * 0.9)
+      .outerRadius(this.radius * 0.9)
 
     this.svg
+      .selectAll('allSlices')
+      .data(data_ready)
+      .enter()
       .append('path')
-      .attr('d', d3.arc()
-        .startAngle(0)
-        .endAngle((partial / total) * (2 * Math.PI))
-        .innerRadius(this.radius * 0.6)
-        .outerRadius(this.radius)
-      )
-      .attr('fill', "#1177dd");
+      .attr('d', arc)
+      .attr("stroke", "white")
+      .style("stroke-width", "2px")
+      .style("opacity", 0.7)
 
+    this.svg
+      .selectAll('allPolylines')
+      .data(data_ready)
+      .enter()
+      .append('polyline')
+      .attr("stroke", "black")
+      .style("fill", "none")
+      .attr("stroke-width", 1)
+      .attr('points', function (d) {
+        var posA = arc.centroid(d)
+        var posB = outerArc.centroid(d)
+
+        return [posA, posB]
+      })
+
+
+    this.svg
+      .selectAll('allLabels')
+      .data(data_ready)
+      .enter()
+      .append('text')
+      .text(function (d) { return d.data.key })
+      .attr('transform', function (d) {
+        var pos = outerArc.centroid(d);
+        var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+        pos[0] += (midangle < Math.PI ? 10 : -10);
+        return 'translate(' + pos + ')';
+      })
+      .style('text-anchor', function (d) {
+        var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+        return (midangle < Math.PI ? 'start' : 'end')
+      })
   }
 }
