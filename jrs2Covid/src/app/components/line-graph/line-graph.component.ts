de7c1545228a2e4b73293648d17edb5f35ef27bc
Will Austell <62250017+Twaustell@users.noreply.github.com>
2021-09-30 14:43:20 -0400

    refactored line graph

--- jrs2Covid/src/app/components/line-graph/line-graph.component.ts
+++ jrs2Covid/src/app/components/line-graph/line-graph.component.ts
@@ -10,34 +10,36 @@ import { WorldometersService } from 'src/app/services/worldometers.service';
 export class LineGraphComponent implements OnInit {
 
   svg;
-  margin = 50;
+  margin = 100;
   width = 750;
-  height = 600;
+  height = 500;
   radius = 250;
 
   data;
+
 
   constructor(private worldService: WorldometersService) { }
 
   ngOnInit(): void {
 
-    this.worldService.getStatesRanked()
+    this.worldService.getSCVaccines()
       .subscribe(data => {
-        this.data = data;
-        console.log(data);
-        this.createSvg();
-        this.drawChart();
+        this.data = data
+        // console.log(data);
+        this.createSvg()
+        this.drawLineGraph(data)
       })
   }
 
   createSvg() {
     this.svg = d3.select(".line-canvas")
       .append("svg")
-      .attr("width", this.width + (this.margin * 2))
-      .attr("height", this.height + (this.margin * 2))
+      .attr("viewBox", [0, 0, this.width, this.height]);
   }
 
   drawChart() {
+
+
     var x = d3.scaleBand()
       .domain(d3.range(this.data.length))
       .range([this.margin, this.width - this.margin])
@@ -66,5 +68,68 @@ export class LineGraphComponent implements OnInit {
       .attr("height", d => y(0) - y(d.cases))
       .attr("width", x.bandwidth())
       .attr('fill', (d) => color(d.cases))
+  }
+
+
+  drawLineGraph(data) {
+
+
+    var vaxData = Object.keys(data.timeline).map(key => {
+      return {
+        date: new Date(key),
+        vaccinations: data.timeline[key]
+      }
+    });
+
+    var vaxExtent = [vaxData[0].vaccinations, vaxData[vaxData.length - 1].vaccinations];
+
+    var y = d3.scaleLinear()
+      .domain(vaxExtent).nice()
+      .range([this.height - this.margin, this.margin])
+
+    var x = d3.scaleLinear()
+      .domain([vaxData[0].date.getTime(), vaxData[vaxData.length - 1].date.getTime()]).nice()
+      .range([this.margin, this.width - this.margin])
+
+    var xAxis = (g) => g
+      .attr("transform", `translate(0,${this.height - this.margin})`)
+      .call(d3.axisBottom(x)
+        .ticks(10)
+        .tickFormat(function (d) {
+          let time = new Date(d)
+          return `${time.getDate()}/${time.getMonth() + 1}/${time.getFullYear()}`;
+        })
+      )
+      .selectAll('text')
+      .attr("transform", "rotate(-45), translate(-8, -5)")
+      .style("text-anchor", "end");
+
+    var yAxis = g => g
+      .attr("transform", `translate(${this.margin},0)`)
+      .call(d3.axisLeft(y))
+
+    this.svg.append("g")
+      .call(xAxis);
+
+    this.svg.append("g")
+      .call(yAxis);
+
+
+    var color = d3.scaleLinear()
+      .domain(vaxExtent)
+      .range(["#000000", "#000000"])
+
+    this.svg.append("path")
+      .datum(vaxData)
+      .attr("fill", "none")
+      .attr("stroke", 'steelblue')
+      .attr("stroke-width", 1.5)
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("d", d3.line()
+        // .defined(d => !isNaN(d.value))
+        .x(d => x(d.date.getTime()))
+        .y(d => y(d.vaccinations))
+      );
   }
 }
