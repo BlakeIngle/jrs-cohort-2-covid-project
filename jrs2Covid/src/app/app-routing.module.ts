b95b83cd3925dd136c9b2894975b68a98a7550b3
fito91jr <rmedinaavalos@gmail.com>
2021-10-01 14:13:13 -0400

    added routing

--- jrs2Covid/src/app/app-routing.module.ts
+++ jrs2Covid/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CountyPageComponent } from './components/county-page/county-page.component';
 import { CovidAppComponent } from './components/covid-app/covid-app.component';
 
 const routes: Routes = [
-  { path: "", component: CovidAppComponent }
+  { path: "", component: CovidAppComponent },
+  { path: ":state/:county", component: CountyPageComponent },
+  { path: ":state/:county/:days", component: CountyPageComponent }
 ];
 
 @NgModule({
