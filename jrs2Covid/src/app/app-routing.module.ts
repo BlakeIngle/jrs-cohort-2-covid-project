cba35459968805eb87d550d910c0f9d3f9b09ad2
Will Austell <62250017+Twaustell@users.noreply.github.com>
2021-10-01 14:12:33 -0400

    routing module commit

--- jrs2Covid/src/app/app-routing.module.ts
+++ jrs2Covid/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CovidAppComponent } from './components/covid-app/covid-app.component';
+import { StatePageComponent } from './components/state-page/state-page.component';
+
+
 
 const routes: Routes = [
-  { path: "", component: CovidAppComponent }
+  { path: "", component: CovidAppComponent },
+  { path: ":state", component: StatePageComponent }
 ];
 
 @NgModule({
