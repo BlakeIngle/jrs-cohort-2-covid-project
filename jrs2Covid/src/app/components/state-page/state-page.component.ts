014ebbf309fb6898710350f583f8f170ae982649
fito91jr <rmedinaavalos@gmail.com>
2021-10-07 14:28:04 -0400

    fixed routing to county pages

--- jrs2Covid/src/app/components/state-page/state-page.component.ts
+++ jrs2Covid/src/app/components/state-page/state-page.component.ts
@@ -64,7 +64,7 @@ export class StatePageComponent implements OnInit {
   }
 
   onCountyClicked() {
-    this.router.navigate([this.state, this.selectedCounty]);
+    this.router.navigate([this.state, this.selectedCounty.toLowerCase()]);
   }
 
 
